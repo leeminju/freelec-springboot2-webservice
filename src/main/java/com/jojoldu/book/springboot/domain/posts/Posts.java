e5e1fb0c9da981caa7bb7af06eb6d462d4c86935
CheezeMJ <leemj4070@gmail.com>
2022-02-10 01:45:03 +0900

    등록,수정,조회 API, 날짜 자동화

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,5 +32,12 @@ public class Posts {
         this.content=content;
         this.author = author;
     }
+
+    public void update(String title,String content)
+    {
+        this.title=title;
+        this.content=content;
+    }
+
 
 }
