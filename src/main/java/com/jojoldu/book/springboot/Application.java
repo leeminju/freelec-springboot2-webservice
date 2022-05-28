151a03e84f29dced317cb90c7859ac7fb1960439
leeminju <leemj4090@naver.com>
2022-05-29 05:20:11 +0900

    수정

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,9 +2,7 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.jdbc.config.annotation.SpringSessionDataSource;
 
-@SpringSessionDataSource
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
